2fca9cc4780eab5fd74f2f0bfd78d75bb67dc080
ssteinmetz <samhsteinmetz@gmail.com>
2024-12-18 18:24:18 -0500

    update homepage and base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://samhsteinmetz.github.io/',
+  base: 'https://samhsteinmetz.github.io/me',
   plugins: [react()],
 })
